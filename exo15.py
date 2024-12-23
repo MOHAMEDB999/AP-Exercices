b0a04c99601a4fd07b43aba4b3bec8218f92a174
Mohamed_Ben <127502254+MOHAMEDB999@users.noreply.github.com>
2024-12-23 17:23:25 +0100

    Create exo15.py

---
+++ exo15.py
@@ -0,0 +1,13 @@
+word = input("Please type in a string: ").lower()
+if("a" in word):
+    print("a is found")
+else:
+    print("a is not found")
+if("e" in word):
+    print("e is found")
+else:
+    print("e is not found")
+if("o" in word):
+    print("o is found")
+else:
+    print("o is not found")
