f3ee8fbb48681507b17b5060f7b8609cb2c0f58a
Mohamed_Ben <127502254+MOHAMEDB999@users.noreply.github.com>
2024-12-23 17:29:26 +0100

    Create exo16.py

---
+++ exo16.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Enter index (-1 to quit): "))
+
+    if index == -1:
+        break
+
+    if 0 <= index < len(numbers):
+        new_value = int(input("Enter new value: "))
+        numbers[index] = new_value
+        print(numbers)
+    else:
+        print("Invalid index. Try again.")
+
+print("Program terminated.")
